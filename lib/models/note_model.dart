c5f5466c23c6d412ccaddb04101ef7485d354b0f
M.Farar <mostafafarar2030@gmail.com>
2025-02-15 17:07:19 +0200

    use hive and generate note_model.g.dart file

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,19 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+part 'note_model.g.dart';
+
+//generate type adpter and this your file name "note_model.g.dart"
+//type id unique per model (0 - 255)
+
+@HiveType(typeId: 1)
+class NoteModel extends HiveObject {
+  //field id unique per class
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subtitle;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
 
   NoteModel(
--- lib/views/notesview.dart
+++ lib/views/notesview.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/widgets/customButtom.dart';
+
 import 'package:notes_app/widgets/customappbar.dart';
 import 'package:notes_app/widgets/showbuttommodelsheet.dart';
 import '../widgets/customlistview.dart';
@@ -10,17 +10,6 @@ class NotesView extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final List<Color> colors = [
-      Colors.red,
-      Colors.blue,
-      Colors.green,
-      Colors.orange,
-      Colors.purple,
-      Colors.yellow,
-      Colors.cyan,
-      Colors.pink,
-      Colors.teal,
-    ];
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(10),
